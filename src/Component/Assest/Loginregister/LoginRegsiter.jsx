4d2979248d8d90918165d66a78d39dcbf20a5904
fuadlouda <fuadlouda@gmail.com>
2024-05-31 17:57:17 +0530

    new added

--- src/Component/Assest/Loginregister/LoginRegsiter.jsx
+++ src/Component/Assest/Loginregister/LoginRegsiter.jsx
@@ -27,65 +27,68 @@ const LoginRegister = ({ onForgotPasswordClick, onLoginSuccess }) => {
   };
 
   return (
-    <div className={`wrapper${action}`}>
-      <div className="form-box login">
-        {!showForgotPassword ? (
-          <form onSubmit={handleLoginSubmit}>
-            <h1>Login</h1>
+    <div>
+      <h1>URL SHORTENER</h1>
+      <div className={`wrapper${action}`}>
+        <div className="form-box login">
+          {!showForgotPassword ? (
+            <form onSubmit={handleLoginSubmit}>
+              <h1>Login</h1>
+              <div className="input-box">
+                <FaUser className="icon" />
+                <input type="text" placeholder="Username" required />
+              </div>
+              <div className="input-box">
+                <FaLock className="icon" />
+                <input type="password" placeholder="Password" required />
+              </div>
+              <div className="remember-forgot">
+                <label>
+                  <input type="checkbox" /> Remember me
+                </label>
+                <a href="#" onClick={handleForgotPasswordClick}>
+                  Forgot password?
+                </a>
+              </div>
+              <button type="submit">Login</button>
+              <div className="register-link">
+                <p>
+                  Don't have an account? <a href="#" onClick={registerLink}>Register</a>
+                </p>
+              </div>
+            </form>
+          ) : (
+            <ForgotPassword onClose={() => setShowForgotPassword(false)} />
+          )}
+        </div>
+        <div className="form-box register">
+          <form action="">
+            <h1>Registration</h1>
             <div className="input-box">
-              <input type="text" placeholder="Username" required />
               <FaUser className="icon" />
+              <input type="text" placeholder="Username" required />
             </div>
             <div className="input-box">
-              <input type="password" placeholder="Password" required />
+              <FaEnvelope className="icon" />
+              <input type="email" placeholder="Email" required />
+            </div>
+            <div className="input-box">
               <FaLock className="icon" />
+              <input type="password" placeholder="Password" required />
             </div>
             <div className="remember-forgot">
               <label>
-                <input type="checkbox" /> Remember me
+                <input type="checkbox" /> I agree to the terms & conditions
               </label>
-              <a href="#" onClick={handleForgotPasswordClick}>
-                Forgot password?
-              </a>
             </div>
-            <button type="submit">Login</button>
+            <button type="submit">Register</button>
             <div className="register-link">
               <p>
-                Don't have an account? <a href="#" onClick={registerLink}>Register</a>
+                Already have an account? <a href="#" onClick={loginLink}>Login</a>
               </p>
             </div>
           </form>
-        ) : (
-          <ForgotPassword onClose={() => setShowForgotPassword(false)} />
-        )}
-      </div>
-      <div className="form-box register">
-        <form action="">
-          <h1>Registration</h1>
-          <div className="input-box">
-            <input type="text" placeholder="Username" required />
-            <FaUser className="icon" />
-          </div>
-          <div className="input-box">
-            <input type="email" placeholder="Email" required />
-            <FaEnvelope className="icon" />
-          </div>
-          <div className="input-box">
-            <input type="password" placeholder="Password" required />
-            <FaLock className="icon" />
-          </div>
-          <div className="remember-forgot">
-            <label>
-              <input type="checkbox" /> I agree to the terms & conditions
-            </label>
-          </div>
-          <button type="submit">Register</button>
-          <div className="register-link">
-            <p>
-              Already have an account? <a href="#" onClick={loginLink}>Login</a>
-            </p>
-          </div>
-        </form>
+        </div>
       </div>
     </div>
   );
